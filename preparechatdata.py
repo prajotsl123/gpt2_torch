bc2e1345cc28407c49ce56a9cde3c1b3c96ce1cf
Prajot S Kuvalekar <59916419+prajotsl123@users.noreply.github.com>
2024-07-07 14:27:36 +0530

    Update preparechatdata.py

--- preparechatdata.py
+++ preparechatdata.py
@@ -10,19 +10,24 @@ data = []
 token_len = []
 block_size = 1024
 for i in trange(ds['train'].__len__()):
+    #prompt : "hi how r u"
+    #response : " hi i m fine"
     text = f"### Prompt: {ds['train'][i]['prompt'].strip()}\n### Response: {ds['train'][i]['response'].strip()}<|endoftext|>"
-    ids = tokenizer(text)['input_ids']
-    mask_ids = tokenizer(f"### Prompt: {ds['train'][i]['prompt'].strip()}")['input_ids']
-    len_mask = len(mask_ids)
-    targets = [-1 for _ in range(len_mask)]
-    if len(ids)>block_size:
-        targets.extend(ids[len_mask+1:block_size+1])
+    ids = tokenizer(text)['input_ids']  # len(ids) = 1+1+4+1+1+4+1 
+    mask_ids = tokenizer(f"### Prompt: {ds['train'][i]['prompt'].strip()}")['input_ids']  #"hi how r u"
+    len_mask = len(mask_ids)  #4
+    targets = [-1 for _ in range(len_mask)]  #[ -1,-1,-1,-1]
+    if len(ids)>block_size: # False
+        targets.extend(ids[len_mask+1:block_size+1]) 
         data.append((ids[:block_size], targets))
         token_len.append(block_size)
     else:
-        targets.extend(ids[len_mask+1:])
+        targets.extend(ids[len_mask+1:]) # [ -1,-1,-1,-1,hi,I , m ,fine]
         data.append((ids[:-1], targets))
         token_len.append(len(ids[:-1]))
  
 with open('chatdata.pkl', 'wb') as f:
     pickle.dump(data, f)
+
+
+
